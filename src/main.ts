62e0a0257bf6a390203e9ae56c27c22fae363e7a
Luz-Davila <luz.davila.mayorga@est.una.ac.cr>
2026-09-13 23:00:31 -0600

    Implementa cambios del Servicio B

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import type { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    rawBody: true,
+  });
+  app.enableCors();
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
